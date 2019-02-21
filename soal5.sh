03ace0e5bc3cc7489d62c9c9be8cb4d6d0394456
Hendra Ramadani <36802769+hendraramadani@users.noreply.github.com>
2019-02-22 06:45:31 +0700

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,3 @@
-4#!/bin/bash
+#!/bin/bash
 
 cat /var/log/syslog | grep -v "sudo" | grep -n "cron" | grep -i "cron" | awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12}' >> ~/modul1/record.txt
